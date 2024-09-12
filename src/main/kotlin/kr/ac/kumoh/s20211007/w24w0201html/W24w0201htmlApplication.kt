9c16224e94171867002a02de148f6c59fbd4e4ef
수환 전 <jsh42672@kumoh.ac.kr>
2024-09-12 14:15:05 +0900

    HTML 시작

---
+++ src/main/kotlin/kr/ac/kumoh/s20211007/w24w0201html/W24w0201htmlApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20211007.w24w0201html
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W24w0201htmlApplication
+
+fun main(args: Array<String>) {
+	runApplication<W24w0201htmlApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20211007/w24w0201html/W24w0201htmlApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20211007.w24w0201html
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W24w0201htmlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
